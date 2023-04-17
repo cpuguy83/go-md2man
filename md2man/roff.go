65a740a5b8948d921db6223577afbe055e892113
Kir Kolyshkin <kolyshkin@gmail.com>
2023-04-17 16:41:28 -0700

    Do not typeset table headings in monospaced font
    Table headings typeset in monospaced font look ugly (this can only be
    seen when rendered into Postscript or PDF). Using bold font is enough.
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- md2man/roff.go
+++ md2man/roff.go
@@ -262,7 +262,7 @@ func (r *roffRenderer) handleTableCell(w io.Writer, node *blackfriday.Node, ente
 			start = "\t"
 		}
 		if node.IsHeader {
-			start += codespanTag
+			start += strongTag
 		} else if nodeLiteralSize(node) > 30 {
 			start += tableCellStart
 		}
@@ -270,7 +270,7 @@ func (r *roffRenderer) handleTableCell(w io.Writer, node *blackfriday.Node, ente
 	} else {
 		var end string
 		if node.IsHeader {
-			end = codespanCloseTag
+			end = strongCloseTag
 		} else if nodeLiteralSize(node) > 30 {
 			end = tableCellEnd
 		}
